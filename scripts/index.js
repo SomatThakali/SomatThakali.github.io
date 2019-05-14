8ab0f6ab831d13ecb0e2f1d56ac683e8f3d19319
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-05-14 15:16:25 -0400

    Added logo in skills section

--- scripts/index.js
+++ scripts/index.js
@@ -197,13 +197,4 @@ $(function() {
   $("#close").click(function() {
     $("#success").removeClass("expand");
   });
-
-  wow = new WOW({
-    boxClass: "wow", // default
-    animateClass: "animated", // default
-    offset: 0, // default
-    mobile: true, // default
-    live: true // default
-  });
-  wow.init();
 });
